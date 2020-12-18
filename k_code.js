9dd99a55080840b2970b740ae92d18bf4b3d5052
F-Wer <43503689+F-Wer@users.noreply.github.com>
2020-12-18 16:39:48 +0100

    deleted all console.log

--- k_code.js
+++ k_code.js
@@ -1,6 +1,5 @@
 document.addEventListener('keydown', event => {
     if(event.isComposing || event.code == 'ArrowUp') {
-        console.log('UP');
         document.addEventListener('keydown', event => {
             if(event.isComposing || event.code == 'ArrowUp') {
                 document.addEventListener('keydown', event => {
@@ -22,18 +21,7 @@ document.addEventListener('keydown', event => {
                                                                                 document.addEventListener('keydown', event => {
                                                                                     if(event.isComposing || event.code == 'Space') {
                                                                                         var rotated = false;
-/* var elements = document.querySelectorAll('body *'), i;
-for (i = 0; i < elements.length; ++i) {
-    console.log(elements);
-    deg = rotated ? 0 : 180;
-    elements[i].style.webkitTransform = 'rotate('+deg+'deg)'; 
-    elements[i].style.mozTransform    = 'rotate('+deg+'deg)'; 
-    elements[i].style.msTransform     = 'rotate('+deg+'deg)'; 
-    elements[i].style.oTransform      = 'rotate('+deg+'deg)'; 
-    elements[i].style.transform       = 'rotate('+deg+'deg)'; 
 
-    rotated = !rotated;
-  } */
   document.body.style.background = "url('https://media1.tenor.com/images/bb6aa5121851f1b57a2cb50f73005c66/tenor.gif?itemid=8375212')";
   hideelements();
   setTimeout(function() {
